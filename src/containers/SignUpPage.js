bfbc2b30d3cdbedb16fa98fe3a38c8ebcbbcb00b
Agustina Chaer <aguschaer@gmail.com>
2018-01-12 12:04:28 -0300

    Added redirect + link to sign in from sign up

--- src/containers/SignUpPage.js
+++ src/containers/SignUpPage.js
@@ -2,24 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link, browserHistory } from 'react-router';
 import * as signUpActions from '../actions/signUpActions';
 import SignUpForm from '../components/user/SignUpForm';
+import { routes } from '../constants/routesPaths';
 
-const SignUpPage = ({ actions: { signUp } }) => (
-  <div>
-    <p>SIGN UP</p>
-    <SignUpForm onSubmit={signUp} />
-  </div>
-);
+const SignUpPage = ({ actions: { signUp }, authenticated }) => {
+  if (authenticated) {
+    browserHistory.push(routes.index);
+  }
+  return (
+    <div>
+      <p>SIGN UP</p>
+      <SignUpForm onSubmit={signUp} />
+      <Link to={routes.login}> Sign in </Link>
+    </div>
+  );
+};
 
-const { object } = PropTypes;
+const { bool, object } = PropTypes;
 
 SignUpPage.propTypes = {
-  actions: object.isRequired
+  actions: object.isRequired,
+  authenticated: bool.isRequired
 };
+
+const mapStateToProps = ({ session: { authenticated } }) => ({
+  authenticated
+});
 
 const mapDispatch = dispatch => ({
   actions: bindActionCreators(signUpActions, dispatch)
 });
 
-export default connect(null, mapDispatch)(SignUpPage);
+export default connect(mapStateToProps, mapDispatch)(SignUpPage);
