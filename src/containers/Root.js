65345f9ea62c2d099fbc1453ad2491f5d18ef4f1
Felipe García <felipe@toptierlabs.com>
2018-04-02 17:32:14 -0300

    Added react-intl to codebase

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,11 +3,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import fr from 'react-intl/locale-data/fr';
+import es from 'react-intl/locale-data/es';
+
+addLocaleData([...en, ...fr, ...es]);
+const usersLocale = navigator.language;
 
 const Root = ({ store, history, routes, render }) => (
-  <Provider store={store}>
-    <Router history={history} routes={routes} render={render} />
-  </Provider>
+  <IntlProvider
+    locale={usersLocale}
+    messages={translationsForUsersLocale}
+  >
+    <Provider store={store}>
+      <Router history={history} routes={routes} render={render} />
+    </Provider>
+  </IntlProvider>
 );
 
 const { object, func } = PropTypes;
