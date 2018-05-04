9e589023b85bf1f48846716fb1005af956b40387
Bernabe <bernabe@rootstrap.com>
2018-05-04 15:53:36 -0300

    Integrate React Helmet

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { bool } from 'prop-types';
 import { ConnectedRouter } from 'react-router-redux';
 import { connect } from 'react-redux';
 import { Switch } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import history from '../utils/history';
 import RouteFromPath from './routes/RouteFromPath';
 import routes from '../routes';
 
 const App = ({ authenticated, checked }) => (
-  <ConnectedRouter history={history}>
-    {checked &&
-      <Switch>
-        {routes.map((route, index) =>
-          <RouteFromPath
-            key={`route${index}`}
-            {...route}
-            authenticated={authenticated}
-          />)
-        }
-      </Switch>
-    }
-  </ConnectedRouter>
+  <Fragment>
+    <Helmet>
+      <title>RS React Redux Base</title>
+    </Helmet>
+    <ConnectedRouter history={history}>
+      {checked &&
+        <Switch>
+          {routes.map((route, index) =>
+            <RouteFromPath
+              key={`route${index}`}
+              {...route}
+              authenticated={authenticated}
+            />)
+          }
+        </Switch>
+      }
+    </ConnectedRouter>
+  </Fragment>
 );
 
 App.propTypes = {
