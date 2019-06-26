a0043fd56521f712e837dea9260bd0c79826285d
Pedro Zunino <peter@rootstrap.com>
2019-06-26 15:34:15 -0300

    Add createAction helper

--- src/reducers/createReducer.js
+++ src/reducers/createReducer.js
@@ -3,4 +3,4 @@ import produce from 'immer';
 export default (initialState, actionHandlers) =>
   (state = initialState, action) =>
     produce(state, draft =>
-      (actionHandlers[action.type] ? actionHandlers[action.type](draft, action) : state));
+      (actionHandlers[action.type] ? actionHandlers[action.type](draft, action.payload) : state));
