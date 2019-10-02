c1047c2a5f3328d8f089b03b45ef0f10cb6ac886
Eugenia <maria.miranda@rootstrap.com>
2019-10-02 15:06:26 -0300

    Created status reducers and their respective constants

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import { sessionReducer as session } from 'redux-react-session';
 import { connectRouter } from 'connected-react-router';
+import actionStatus from 'reducers/statusReducer';
+
 
 const rootReducer = history =>
   combineReducers({
     form,
     session,
+    actionStatus,
     router: connectRouter(history)
   });
 
