1271a22d0546b25dde945173d043453edc65bfcd
Eugenia <maria.miranda@rootstrap.com>
2019-10-02 15:06:58 -0300

    Created status and loading hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,4 @@
 export { default as useSession } from './useSession';
 export { default as useDispatch } from './useDispatch';
+export { default as useLoading } from './useLoading';
+export { default as useStatus } from './useStatus';
