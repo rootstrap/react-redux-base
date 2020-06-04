e5dbf1c1a48a1ab86ce59d761d5afc9046f49abe
Pedro Zunino <peter@rootstrap.com>
2020-06-04 14:14:13 -0300

    Remove styled components

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,30 @@ import React from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { ThemeProvider } from 'styled-components';
 
 import { useSession } from 'hooks';
 import history from 'utils/history';
 import RouteFromPath from 'components/routes/RouteFromPath';
 import routes from '../routes';
-import theme from '../constants/theme';
 import Header from './common/Header';
 
 const App = () => {
   const { authenticated } = useSession();
 
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <Helmet>
-          <title>RS React Redux Base</title>
-        </Helmet>
-        <Header />
-        <ConnectedRouter history={history}>
-          <Switch>
-            {routes.map((route, index) => (
-              <RouteFromPath key={`route${index}`} {...route} authenticated={authenticated} />
-            ))}
-          </Switch>
-        </ConnectedRouter>
-      </>
-    </ThemeProvider>
+    <>
+      <Helmet>
+        <title>RS React Redux Base</title>
+      </Helmet>
+      <Header />
+      <ConnectedRouter history={history}>
+        <Switch>
+          {routes.map((route, index) => (
+            <RouteFromPath key={`route${index}`} {...route} authenticated={authenticated} />
+          ))}
+        </Switch>
+      </ConnectedRouter>
+    </>
   );
 };
 
--- src/utils/testHelpers.js
+++ src/utils/testHelpers.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { IntlProvider } from 'react-intl';
 import { MemoryRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 
 import locales from 'locales';
-import theme from 'constants/theme';
 
 const messages = locales.en;
 
 export const withStore = (WrappedComponent, store) => (
   <MemoryRouter>
-    <ThemeProvider theme={theme}>
-      <IntlProvider locale="en" messages={messages}>
-        <Provider store={store}>{WrappedComponent}</Provider>
-      </IntlProvider>
-    </ThemeProvider>
+    <IntlProvider locale="en" messages={messages}>
+      <Provider store={store}>{WrappedComponent}</Provider>
+    </IntlProvider>
   </MemoryRouter>
 );
